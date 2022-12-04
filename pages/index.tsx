02a539cf43b9b84559516bc2f84587541c79154a
FalseMann <false@falsemann.com>
2022-12-03 18:04:49 -0800

    feat: create initial project structure

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center h-screen">
+      <div className="container mx-auto text-center text-9xl">nuIlzero</div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        midnight: {
+          100: "#1A1A23",
+          200: "#15151D",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
