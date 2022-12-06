5fc4cdf1298c829dab56bbbfd90084a0d317c3c2
FalseMann <false@falsemann.com>
2022-12-05 21:22:15 -0800

    feat: reconfigure homepage for initial release
    "Movie Night" and "Login with Twitch" buttons are hidden, and links to
    external sites are added.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const APP_HOSTNAME =
@@ -6,18 +7,24 @@ const APP_HOSTNAME =
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto w-3/4 pt-20 pb-32 sm:pt-28 sm:pb-40">
-        <div className=" flex mb-5">
-          <div className="flex ">
-            <div className="ml-auto mx-10 text-sm text-red-400">nuilzero</div>
-            <div className="mx-10 text-sm">Movie Night</div>
-            <div className="mx-10 text-sm">Discord</div>
-            <div className="mx-10 text-sm">Subscribe</div>
+      <div className="container w-3/4 pt-20 pb-32 mx-auto sm:pt-28 sm:pb-40">
+        <div className="flex mb-5">
+          <div className="flex space-x-8">
+            <div className="ml-auto text-base font-bold text-red-400">
+              <Link href="/">nuilzero</Link>
+            </div>
+            {/* <div className="text-sm">Movie Night</div> */}
+            <div className="text-xs font-bold leading-6">
+              <a href="https://discord.gg/xzrj9FfszD">Discord</a>
+            </div>
+            <div className="text-xs font-bold leading-6">
+              <a href="https://www.twitch.tv/products/nuilzero">Subscribe</a>
+            </div>
           </div>
 
-          <div className="ml-auto flex-end text-sm border-solid bg-twitchColor rounded-md">
+          {/* <div className="ml-auto text-sm border-solid rounded-md flex-end bg-twitchColor">
             <button className="m-1.5">Login With Twitch</button>
-          </div>
+          </div> */}
         </div>
 
         <div id="twitch-player">
