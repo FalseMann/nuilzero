001e2f12915bd7536eb092fb85a3fa73d2a0f91a
kc <swokowski11@hotmail.com>
2022-12-09 17:14:10 -0600

    get database working

--- prisma/prismaScript.ts.mjs
+++ prisma/prismaScript.ts.mjs
@@ -1,23 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 async function main() {
-  ///. write prisma client queries here
+  // ... you will write your Prisma Client queries here
   const user = await prisma.user.create({
     data: {
-      email: "test@nuilzero.com",
       name: "Nuilzero",
-      // posts: [],
-      // profile: "Nuilzero's Test",
+      email: "nuilzero@prisma.io",
     },
   });
-  console.log("user", user);
+  console.log(user);
 }
 
 main()
-  .catch((e) => {
-    console.log(e.message);
-  })
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
