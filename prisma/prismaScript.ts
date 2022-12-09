9d09ab5aeb9525763163fc2a6346604af67aaf88
kc <swokowski11@hotmail.com>
2022-12-09 16:14:23 -0600

    get fake user made with npm run prismaStart

--- prisma/prismaScript.ts
+++ prisma/prismaScript.ts
@@ -3,6 +3,15 @@ const prisma = new PrismaClient();
 
 async function main() {
   ///. write prisma client queries here
+  const user = await prisma.user.create({
+    data: {
+      email: "test@nuilzero.com",
+      name: "Nuilzero",
+      // posts: [],
+      // profile: "Nuilzero's Test",
+    },
+  });
+  console.log("user", user);
 }
 
 main()
