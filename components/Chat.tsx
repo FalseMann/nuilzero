1b17d91b3ea6130109439e1314c3a9bf5ae09b2c
FalseMann <false@falsemann.com>
2022-12-09 22:49:23 -0800

    refactor: break up UI into smaller components

---
+++ components/Chat.tsx
@@ -0,0 +1,12 @@
+interface ChatProps {
+  appHostname: string;
+}
+
+export function Chat({ appHostname }: ChatProps) {
+  return (
+    <iframe
+      className="relative float-right w-1/4 h-full"
+      src={`https://www.twitch.tv/embed/nuilzero/chat?parent=${appHostname}`}
+    ></iframe>
+  );
+}
---
+++ components/VideoPlayer.tsx
@@ -0,0 +1,15 @@
+interface VideoPlayerProps {
+  appHostname: string;
+}
+
+export function VideoPlayer({ appHostname }: VideoPlayerProps) {
+  return (
+    <iframe
+      className="relative float-left w-3/4 h-full "
+      src={`https://player.twitch.tv/?channel=nuilzero&parent=${appHostname}`}
+      frameBorder="0"
+      allowFullScreen={true}
+      scrolling="no"
+    ></iframe>
+  );
+}
