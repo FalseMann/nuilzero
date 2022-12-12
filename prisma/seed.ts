f854da2ac5654e3190738abd34c31ce300a23dc4
FalseMann <false@falsemann.com>
2022-12-11 22:19:43 -0800

    feat: add mysql integration

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const donkey = await prisma.user.upsert({
+    where: { email: "donkey@kong.com" },
+    update: {},
+    create: {
+      email: "donkey@kong.com",
+      name: "Donkey Kong",
+    },
+  });
+  const diddy = await prisma.user.upsert({
+    where: { email: "diddy@kong.com" },
+    update: {},
+    create: {
+      email: "diddy@kong.com",
+      name: "Diddy Kong",
+    },
+  });
+  console.log({ donkey, diddy });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
