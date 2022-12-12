e5e15bcd43ce4c9d0de623a53c9f81a20462bd76
FalseMann <false@falsemann.com>
2022-12-11 16:05:58 -0800

    feat: add redis integration

---
+++ lib/config.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+const ConfigSchema = z.object({
+  redis: z.object({
+    host: z.string(),
+    port: z.coerce.number(),
+    password: z.string(),
+    tls: z.boolean(),
+  }),
+});
+
+const config = ConfigSchema.parse({
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    tls: process.env.REDIS_TLS !== "false",
+  },
+});
+
+export default config;
