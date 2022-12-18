c6fdca6bdf984cad30ffdaf87250a1b40484f0a3
FalseMann <false@falsemann.com>
2022-12-17 21:45:00 -0800

    style: make footer link slightly smaller than normal text

--- components/GlobalFooter.tsx
+++ components/GlobalFooter.tsx
@@ -4,9 +4,11 @@ export function GlobalFooter() {
   return (
     <div className="absolute inset-x-0 bottom-0 w-full border-black-[1px] bg-midnight-200">
       <div className="container mx-auto m-10 w-3/4 text-footerText">
-        nuilzero.tv &copy;
+        <span className="font-semibold">nuilzero.tv &copy;</span>
         <br />
-        <a href="/twitch">twitch.tv/nuilzero</a>
+        <a href="/twitch" className="text-sm">
+          twitch.tv/nuilzero
+        </a>
       </div>
     </div>
   );
