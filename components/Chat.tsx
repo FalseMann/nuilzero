c09d8481ab993e03320d3d80015162ba60518062
FalseMann <false@falsemann.com>
2022-12-18 14:05:10 -0800

    feat: use dark mode for chat embed

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -6,7 +6,7 @@ export function Chat({ appHostname }: ChatProps) {
   return (
     <iframe
       className="relative float-right w-1/4 h-full"
-      src={`https://www.twitch.tv/embed/nuilzero/chat?parent=${appHostname}`}
+      src={`https://www.twitch.tv/embed/nuilzero/chat?parent=${appHostname}&darkpopout=1`}
     ></iframe>
   );
 }
