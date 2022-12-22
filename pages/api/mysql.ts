f1994f15207547a948521e7a668c954091d1eeaa
FalseMann <false@falsemann.com>
2022-12-21 16:14:17 -0800

    chore: remove debug logging messages

--- pages/api/mysql.ts
+++ pages/api/mysql.ts
@@ -2,7 +2,6 @@ import { User } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../lib/db/postgres";
 
-console.log("DATABASE_URL", process.env.DATABASE_URL);
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<User[]>
