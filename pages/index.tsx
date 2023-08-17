6c9f524b99d664917a110e166d3160d702ef1116
Ben Truyman <ben@truyman.com>
2023-08-17 09:19:02 -0700

    the end

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import { VideoPlayer } from "../components/VideoPlayer";
-import { Chat } from "../components/Chat";
-import { MainLayout } from "../components/layouts/MainLayout";
-import config from "../lib/browser/config";
-
 export default function Home() {
   return (
-    <MainLayout>
-      <VideoPlayer appHostname={config.env.hostname} />
-      <Chat appHostname={config.env.hostname} />
-    </MainLayout>
+    <div className="video-container">
+      <iframe
+        title="YouTube Video"
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/nfRlrV8awo0?autoplay=1&controls=0&showinfo=0&loop=1"
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 }
